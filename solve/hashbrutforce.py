cb2d6c62eec23d0d654af8ae440968639bbcd7ee
Holger Klausnitzer <hok@bankdata.dk>
2025-07-04 14:31:50 +0200

    reverse hash added

--- solve/hashbrutforce.py
+++ solve/hashbrutforce.py
@@ -7,7 +7,6 @@ def bruteforce_sha256(target_hash):
             for day in range(1, 32):
                 try:
                     candidate = f"{year:04d}{month:02d}{day:02d}"
-                    print(f"candidate: {candidate}")
                     h = hashlib.sha256(candidate.encode()).hexdigest()
                     attempts += 1
                     if h == target_hash:
@@ -19,7 +18,7 @@ def bruteforce_sha256(target_hash):
     return None
 
 if __name__ == "__main__":
-    target_hash = "1837f05e00bd84bb5a2e637f54ea6990493d589e9288028759c800d3329bd15b"
+    target_hash = "30e6658e2326d26a1d6ba0dedbb2499fea570362a1ed19e6c2949afd8a458814"
     result = bruteforce_sha256(target_hash)
     if result:
         print("Found:", result)
